9475e9e17e162cb64f00675e55a9fba3555b4b9c
Konstantin Senchenkov <senchenkovko@gmail.com>
2023-11-19 13:41:17 +0400

    Create New HW_2

--- Editor3D/PYTHON/ModelElements/poligonal_model.py
+++ Editor3D/PYTHON/ModelElements/poligonal_model.py
@@ -1,8 +1,9 @@
 from .poligon import Poligon
 from .texture import Texture
-
+from Stuff.point_3d import Point3D
 
 class PoligonalModel:
-    def __init__(self, texture: list[Texture]) -> None:
-        self.poligons: list[Poligon] = [Poligon()]
-        self.texture: list[Texture] = texture
+    def __init__(self, texture):
+        self.Poligons = []
+        self.Texture = texture
+        self.poligons.append(Poligon(Point3D()))
--- Editor3D/PYTHON/ModelElements/scene.py
+++ Editor3D/PYTHON/ModelElements/scene.py
@@ -1,23 +1,16 @@
-from ModelElements.camera import Camera
-from ModelElements.flash import Flash
-from ModelElements.poligonal_model import PoligonalModel
-from typing import List, TypeVar
 from abc import ABC
 
 
-T = TypeVar('T', bound='PoligonalModel')
-
-
 class Scene(ABC):
-    def __init__(
-        self, models: List[T], flashes: List[Flash], cameras: List[Camera]
-    ) -> None:
-        self.id: int = 0
-        self.models: List[T] = models
-        self.flashes: List[Flash] = flashes
-
-    def method1(self, obj: T) -> T:
-        return obj
-
-    def method2(self, obj1: T, obj2: T) -> T:
-        return obj1
+    def __init__(self, id, models, flashes, cameras):
+        self.ID = id
+        if len(models) > 0:
+            self.Models = models
+        else:
+            raise Exception("Only one model must be")
+        self.Flashes = flashes
+        if len(cameras) > 0:
+            self.cameras = cameras
+        else:
+            raise Exception("Only one camera must be")
+        
